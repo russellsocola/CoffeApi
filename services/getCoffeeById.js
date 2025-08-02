11302a4be141a1cda48a5f4cd1e6d76f80a0f191
russellsocola <russell999nahum@gmail.com>
2025-08-01 21:48:08 -0500

    Fix Feature getCoffee by Id

--- services/getCoffeeById.js
+++ services/getCoffeeById.js
@@ -1,47 +1,49 @@
-const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb")
+const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
 
 const client = new DynamoDBClient({});
-exports.handler = async(event) => {
-    try{
-        const coffeeId = event?.pathParameters?.id;
-        
-        console.log("Input: ",coffeeId)
-        const params = {
-            TableName: "E_INVOICE",
-            Key: {
-                id: {S: coffeeId}
-            }
-        };
-        try{
-            const commad = new GetItemCommand(params);
-            const data = await client.send(commad);
 
-            console.log("response de data: ", data);
-            if (!data.Item){    
-                return{
-                    statusCode: 404,
-                    body: JSON.stringify({error: "Invoice no found"})
-                };
-            };
+exports.handler = async (event) => {
+  try {
+    const coffeeId = event?.pathParameters?.id;
 
-            return {
-                statusCode: 200,
-                body: JSON.stringify(data.Item)
-            };
-        }catch(error){
-            
-            console.error("Error querying DynamoDB", error);
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: "Failed to query DynamoDB" })
-            };
-        }
+    console.log("Input ID: ", coffeeId);
 
-    }catch(error){
-        console.log("Error: ", error);
-        return{
-            statusCode: 500,
-            body: JSON.stringify({message: "Internal Server Error"})
-        }
+    if (!coffeeId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Missing ID in path" })
+      };
     }
-}
+
+    const params = {
+      TableName: "E_INVOICE",
+      Key: {
+        id: { S: coffeeId }
+      }
+    };
+
+    const command = new GetItemCommand(params);
+    const data = await client.send(command);
+
+    console.log("DynamoDB response: ", data);
+
+    if (!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Invoice not found" })
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Item)
+    };
+
+  } catch (error) {
+    console.error("Error: ", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" })
+    };
+  }
+};
