8a2d57a1291b5c5ca46799f2b6f3cc7af546a315
russellsocola <russell999nahum@gmail.com>
2025-08-03 23:43:38 -0500

    Fix feature

--- services/createCoffee.js
+++ services/createCoffee.js
@@ -1,4 +1,4 @@
-const { ddbDocClient,PutCommand } = require("..ddbclient"); 
+const { ddbDocClient,PutCommand } = require("../ddbclient"); 
 const { v4: uuidv4 } = require("uuid");
 
 exports.handler = async(event) => {
